c3ff68bfd8c0a57cf7399da29f0ded13f9abfda5
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-10-08 20:09:27 -0400

    added useeffect to mirror componentdidmount and unmount lifecycle methods

--- src/components/VideoRoomComponent.js
+++ src/components/VideoRoomComponent.js
@@ -27,4 +27,20 @@ export default VideoRoomComponent = () => {
   const [token, setToken] = useState(undefined);
   // const [chatDisplay, setChatDisplay] = useState("none"); not video/audio reqs
   // STATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // componentDidMount
+  useEffect(() => {
+    const openViduLayoutOptions = {
+      maxRatio: 3 / 2, // The narrowest ratio that will be used (default 2x3)
+      minRatio: 9 / 16, // The widest ratio that will be used (default 16x9)
+      fixedRatio: false, // If this is true then the aspect ratio of the video is maintained and minRatio and maxRatio are ignored (default false)
+      bigClass: "OV_big", // The class to add to elements that should be sized bigger
+      bigPercentage: 0.8, // The maximum percentage of space the big ones should take up
+      bigFixedRatio: false, // fixedRatio for the big ones
+      bigMaxRatio: 3 / 2, // The narrowest ratio to use for the big elements (default 2x3)
+      bigMinRatio: 9 / 16, // The widest ratio to use for the big elements (default 16x9)
+      bigFirst: true, // Whether to place the big one in the top left (true) or bottom right
+      animate: true, // Whether you want to animate the transitions
+    };
+
 };
