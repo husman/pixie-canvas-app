16c11cc9de9bbc1ae51fd24f8e141d347a986374
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-10-15 20:07:52 -0400

    fixed video ref, cleaned up logic

--- src/components/stream/OvVideo.js
+++ src/components/stream/OvVideo.js
@@ -1,19 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import "./StreamComponent.css";
 
-export default function OvVideo({
-  user,
-  mutedSound,
-}) {
+export default function OvVideo({ user, mutedSound }) {
   const videoRef = useRef();
   const stream = user.getStreamManager();
-
   useEffect(() => {
-    if (stream && user && user.streamManager && !!videoRef) {
-      console.log("PROPS: ", user.streamManager);
+    if (stream && !!videoRef) {
       stream.addVideoElement(videoRef.current);
 
-      if (user && user.streamManager.session) {
+      console.log("STREAM ADD VIDE EL===============", user.streamManager);
+
+      if (user.streamManager.session) {
         user.streamManager.session.on("signal:userChanged", (event) => {
           const data = JSON.parse(event.data);
           if (data.isScreenShareActive !== undefined) {
@@ -21,22 +18,14 @@ export default function OvVideo({
           }
         });
       }
-    } else if (stream && !!videoRef) {
-      stream.addVideoElement(videoRef.current);
     }
   });
-
-  console.log("FDTMH", user);
-
-  if (!stream) {
-    return null;
-  }
 
   return (
     <video
       ref={videoRef}
       autoPlay={true}
-      id={"video-" + stream.streamId}
+      id={"video-" + stream.stream.streamId}
       muted={mutedSound}
     />
   );
