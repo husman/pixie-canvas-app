cf38bc73881a2decc7bce68f2590c1d148c665ba
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-11-14 18:27:26 -0500

    added constants for openvidu signals

---
+++ src/components/constants/signals.js
@@ -0,0 +1,4 @@
+export const STREAM_CREATED = "streamCreated";
+export const SIGNAL_USER_CHANGED = "signal:userChanged";
+export const STREAM_DESTROYED = "streamDestroyed";
+export const USER_CHANGED = "userChanged";
