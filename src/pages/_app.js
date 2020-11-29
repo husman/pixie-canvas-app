d7bc266bce772ca18448660621f21e7ab0294711
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-11-28 20:06:43 -0500

    updated styles route

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import '../components/videoroom/VideoRoom.css';
-import '../../thirdparty/impress-common.css';
-import '../../thirdparty/classic-slides.css';
+import React from "react";
+import "../styles/index.css";
+import "../../thirdparty/impress-common.css";
+import "../../thirdparty/classic-slides.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
