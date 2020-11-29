41c0cedeae805760b3b7a3783a12ec18700204e7
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-11-28 20:08:01 -0500

    added join meeting logic to video route

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import VideoRoom from '../components/videoroom/VideoRoom';
-import OvContext from '../context/openVidu';
+import React, { useEffect, useState } from "react";
+import VideoRoom from "../components/VideoRoom";
+import JoinMeetingForm from "../components/JoinMeetingForm";
+import OvContext from "../context/openVidu";
 
 export default function Video() {
+  const [sessionId, setSessionId] = useState("");
   const [openVidu, setOpenVidu] = useState(null);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('openvidu-browser').then(ov => setOpenVidu(new ov.OpenVidu()));
+    if (typeof window !== "undefined") {
+      import("openvidu-browser").then((ov) => setOpenVidu(new ov.OpenVidu()));
     }
   }, []);
 
@@ -17,7 +19,11 @@ export default function Video() {
 
   return (
     <OvContext.Provider value={openVidu}>
-      <VideoRoom />
+      {!sessionId ? (
+        <JoinMeetingForm onSubmit={setSessionId} />
+      ) : (
+        <VideoRoom sessionId={sessionId} />
+      )}
     </OvContext.Provider>
   );
 }
