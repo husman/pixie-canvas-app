bce318335bb26400aa256ebf8cd603be6e8efb44
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-12-04 12:25:18 -0500

    replaced hardcoded values for max videos in container with constants

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Stream from "./Stream";
+import {
+  DEFAULT_MIN_VIDEOS,
+  DEFAULT_MID_MAX_VIDEOS,
+  DEFAULT_MAX_VIDEOS,
+} from "./constants/video";
 
 const Videos = ({ stream, isMicOn, isCameraOn, subscribers }) => {
   const subscribersCt = Object.keys(subscribers).length;
   let videoLayout = "one";
 
-  if (subscribersCt >= 1 && subscribersCt < 4) {
+  if (
+    subscribersCt >= DEFAULT_MIN_VIDEOS &&
+    subscribersCt < DEFAULT_MID_MAX_VIDEOS
+  ) {
     videoLayout = "two";
-  } else if (subscribersCt >= 4) {
+  } else if (
+    subscribersCt >= DEFAULT_MID_MAX_VIDEOS &&
+    subscribersCt <= DEFAULT_MAX_VIDEOS
+  ) {
     videoLayout = "three";
   }
 
@@ -23,7 +34,7 @@ const Videos = ({ stream, isMicOn, isCameraOn, subscribers }) => {
         {Object.entries(subscribers).map(([key, value]) => (
           <Stream
             stream={value.stream}
-            className="subscriber-stream"
+            className="user-stream"
             key={key}
             isMicOn={value.isMicOn}
             isCameraOn={value.isCameraOn}
