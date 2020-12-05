a0e3144e86b0c1967ab7b2996c85d9bf9d458e88
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-12-04 19:55:33 -0500

    updated insert mode to replace

--- src/components/constants/video.js
+++ src/components/constants/video.js
@@ -1,5 +1,5 @@
 export const RESOLUTION = "1280x720";
-export const INSERT_MODE = "APPEND";
+export const INSERT_MODE = "REPLACE";
 export const VIDEO_FRAME_RATE = 30;
 export const DEFAULT_USERNAME = "USER";
 export const DEFAULT_MIN_VIDEOS = 1;
